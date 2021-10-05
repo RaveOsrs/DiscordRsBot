3287b56aa93bb77336d86c2f339b452ebbd20004
Per Slegt <per.slegt@gmail.com>
2021-10-05 16:03:36 +0200

    bugfix

--- reactionRoles.js
+++ reactionRoles.js
@@ -1,5 +1,5 @@
-const {Client, Intents} = require('discord.js');
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL", "MESSAGE", "REACTION"] });
+const Discord = require('discord.js');
+const bot = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 const axios = require('axios').default;
 const config = require('./config.json');
 
