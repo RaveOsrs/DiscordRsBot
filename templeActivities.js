36770f07164683d35f72905452aad1bfb79e580a
Per Slegt <per.slegt@gmail.com>
2021-10-10 14:58:21 +0200

    added fetch

--- templeActivities.js
+++ templeActivities.js
@@ -1,7 +1,6 @@
 const {Client, Intents} = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
-const axios = require('axios').default;
-const config = require('./config.json');
+import fetch from "node-fetch";
 
 bot.once('ready', () =>{
     console.log('templeActivites is online!');
@@ -9,40 +8,40 @@ bot.once('ready', () =>{
 
 bot.on('message', message =>{
     const HOUR = 2000 * 60 *60;
-    if(!message.channel.name == "bot-testing") return;
+    if (!message.channel.name == "bot-testing") return;
+    if (message.author.bot) return;
     message.channel.send("respond!");
 
-    setInterval(() => {
-        const anHourAgo = Date.now() - HOUR;
-        console.log('in the interval');
-        try {
-            const response = fetch("https://templeosrs.com/api/group_achievements.php?id=1386")
-                .then(data => response.json());
-            console.log(data);
+    const anHourAgo = Date.now() - HOUR;
+    console.log('in the interval');
+    try {
+        const response = fetch("https://templeosrs.com/api/group_achievements.php?id=1386")
+            .then(data => response.json());
+        console.log(data);
 
-            for (activity in data) {
-                var posted = new Date(activity.Date);
-                console.log(activity);
+        for (activity in data) {
+            var posted = new Date(activity.Date);
+            console.log(activity);
+            console.log(posted + " - " + anHourAgo);
+            if (posted > anHourAgo) {
+                const embed = new MessageEmbed();
                 console.log(posted + " - " + anHourAgo);
-                if (posted > anHourAgo) {
-                    const embed = new MessageEmbed();
-                    console.log(posted + " - " + anHourAgo);
-                    switch(activity.Type) {
-                        case "Skill":
-                            embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} in ${activity.Skill}!`);
-                            break;
-                        case "Pvm":
-                            embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} ${activity.Skill} kills!`);
-                            break;
-                    }
-                    message.channel.send(embed);
+                switch(activity.Type) {
+                    case "Skill":
+                        embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} in ${activity.Skill}!`);
+                        break;
+                    case "Pvm":
+                        embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} ${activity.Skill} kills!`);
+                        break;
                 }
+                message.channel.send(embed);
             }
-        } catch (error) {
-            message.channel.send('Oops, there was an error fetching the API');
-            console.log(error);
         }
-    }, 60000);
+    } catch (error) {
+        message.channel.send('Oops, there was an error fetching the API');
+        console.log(error);
+    }
+
 });
 
 function numberWithCommas(x) {
