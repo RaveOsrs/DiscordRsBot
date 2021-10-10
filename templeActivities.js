b62cd48ddb150a02315c1506491dccf6af2f510a
Per Slegt <per.slegt@gmail.com>
2021-10-10 19:17:35 +0200

    fix

--- templeActivities.js
+++ templeActivities.js
@@ -14,26 +14,28 @@ bot.on('message', async message =>{
     const anHourAgo = Date.now() - HOUR;
     try {
         let {file} = await fetch("https://templeosrs.com/api/group_achievements.php?id=1386")
-            .then(response => response.json());
-
-        for (activity in response) {
-            var posted = new Date(activity.Date);
-            console.log(activity);
-            console.log(posted + " - " + anHourAgo);
-            if (posted > anHourAgo) {
-                const embed = new MessageEmbed();
-                console.log(posted + " - " + anHourAgo);
-                switch(activity.Type) {
-                    case "Skill":
-                        embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} in ${activity.Skill}!`);
-                        break;
-                    case "Pvm":
-                        embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} ${activity.Skill} kills!`);
-                        break;
+            .then(response => response.json())
+            .then(response => {
+                for (activity in response) {
+                    var posted = new Date(activity.Date);
+                    console.log(activity);
+                    console.log(posted + " - " + anHourAgo);
+                    if (posted > anHourAgo) {
+                        const embed = new MessageEmbed();
+                        console.log(posted + " - " + anHourAgo);
+                        switch(activity.Type) {
+                            case "Skill":
+                                embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} in ${activity.Skill}!`);
+                                break;
+                            case "Pvm":
+                                embed.setDescription(`**${activity.Username}**, Reached ${numberWithCommas(activity.Xp)} ${activity.Skill} kills!`);
+                                break;
+                        }
+                        message.channel.send(embed);
+                    }
                 }
-                message.channel.send(embed);
-            }
-        }
+            })
+        
     } catch (error) {
         message.channel.send('Oops, there was an error fetching the API');
         console.log(error);
