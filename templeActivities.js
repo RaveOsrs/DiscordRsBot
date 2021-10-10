48c817d6b68365bab286a0a39754adafd4acb1d1
Per Slegt <per.slegt@gmail.com>
2021-10-10 13:25:49 +0200

    changed top const's

--- templeActivities.js
+++ templeActivities.js
@@ -1,7 +1,5 @@
-const {Client, Intents} = require('discord.js');
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
-const axios = require('axios').default;
-const config = require('./config.json');
+const discord = require('discord.js');
+const bot = new discord.Client();
 
 bot.once('ready', () =>{
     console.log('templeActivites is online!');
