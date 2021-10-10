340f28c1fd820f1c1b6236f44b602869e73d5121
Per Slegt <per.slegt@gmail.com>
2021-10-10 18:26:53 +0200

    fix

--- templeActivities.js
+++ templeActivities.js
@@ -1,6 +1,6 @@
 const {Client, Intents} = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
-var fetch = import("node-fetch");
+var fetch = import("./node_modules/node-fetch");
 
 bot.once('ready', () =>{
     console.log('templeActivites is online!');
