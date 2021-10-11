4bcd6d69faf081ed5239cf2e01193dac3d8038e8
Per Slegt <per.slegt@gmail.com>
2021-10-11 19:37:02 +0200

    to once instead of on message

--- templeActivities.js
+++ templeActivities.js
@@ -3,11 +3,50 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const fetch = require('cross-fetch');
 const {TOKEN} = require('./config.json');
 
-client.once('ready', () =>{
-    console.log('templeActivites is online!');
+client.once('ready', () =>{    
+    const channel = client.channels.cache.find(channel => channel.name == "general");
+    if (message.author.bot) return;
+
+    const minutesAgo = new Date();
+    minutesAgo.setMinutes(minutesAgo.getMinutes() - 5);
+    setInterval(async function() {
+        try {
+            fetch("https://templeosrs.com/api/group_achievements.php?id=1386")
+            .then(response => response.json())
+            .then(data => {
+                for (x in data) {
+                    console.log("Checking TempleOSRS");
+                    for (activity in data[x]) {
+                        var posted = new Date(data[x][activity].Date);
+                        if (posted > minutesAgo) {
+                            switch(data[x][activity].Type) {
+                                case "Skill":
+                                    console.log("skill");
+                                    //embed.setDescription("**"+ data[x][activity].Username+"** Reached "+data[x][activity].Xp + data[x][activity].Skill+"!");
+                                    channel.send(`**${data[x][activity].Username}**, Reached ${numberWithCommas(data[x][activity].Xp)}xp in ${data[x][activity].Skill}!`);
+                                    setTimeout(() => {  }, 30000);
+                                    break;
+                                case "Pvm":
+                                    console.log("pvm");
+                                    //embed.setDescription("**"+ data[x][activity].Username+"** Reached "+data[x][activity].Xp + data[x][activity].Skill+" kills!");
+                                    channel.send(`**${data[x][activity].Username}**, Reached ${numberWithCommas(data[x][activity].Xp)} ${data[x][activity].Skill} kills!`);
+                                    setTimeout(() => {  }, 30000);
+                                    break;
+                                default:
+                                    channel.send(`Uhh this is not supposed to happen :(`);
+                            }
+                        }
+                    }
+                }
+            })
+        }catch (error) {
+            message.channel.send('Oops, there was an error fetching the API');
+            console.log(error);
+        }
+    }, 5 * 60 * 1000)
 });
 
-client.on('message', async message =>{
+/*client.on('message', async message =>{
     const channel = client.channels.cache.find(channel => channel.name == "general");
     if (message.author.bot) return;
 
@@ -48,7 +87,7 @@ client.on('message', async message =>{
             console.log(error);
         }
     }, 5 * 60 * 1000)
-});
+});*/
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
