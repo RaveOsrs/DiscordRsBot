0b91b7036d2d77e3198f58888effa0b8392e1a47
Per Slegt <per.slegt@gmail.com>
2021-10-27 15:55:54 +0200

    import collection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const {Client, Intents} = require('discord.js');
+const {Client, Collection, Intents} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
 
 client.commands = new Collection();
