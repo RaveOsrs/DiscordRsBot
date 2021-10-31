b32bdbdcfbdf3f29c1eb6be35e489576b876fd73
Per Slegt <per.slegt@gmail.com>
2021-10-31 22:44:48 +0100

    builder update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const {Client, Collection, Intents} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
 
-/*client.commands = new Collection();
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -10,13 +10,13 @@ for (const file of commandFiles) {
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
-}*/
+}
 
 client.once('ready', () =>{
     console.log('RaveBot is online!');
 });
 
-/*client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	const command = client.commands.get(interaction.commandName);
@@ -29,7 +29,7 @@ client.once('ready', () =>{
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-});*/
+});
 
 postrsnRequire = require('./postRsn.js');
 namechangeRequire = require("./changeName.js");
