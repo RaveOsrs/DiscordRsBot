1db1688d5275e06ef3c9ca1be38052646d29716c
Per Slegt <per.slegt@gmail.com>
2021-11-08 16:58:00 +0100

    profile js

---
+++ commands/profile.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const admin = require('firebase-admin');
+
+const DB = admin.database();
+
+const wins;
+const id;
+let rsn;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('profile')
+		.setDescription('Lookup a profile')
+        .addStringOption(option =>
+            option.setName('rsn')
+            .setDescription('User u want to check.')
+            .setRequired(false)),
+	async execute(interaction) {
+        if (!rsn) {
+            rsn = interaction.member.displayName.toLowerCase()
+        }
+        rsn = interaction.options.getString('rsn');
+
+        DB.ref('users/'+rsn).once('value').then(function(snapshot) {
+            wins = snapshot.val().compWins.toString();
+            id = snapshot.val().userId.id.toString();
+        })
+        .then(interaction.reply(`Wins:${wins}, disc ID: ${id})`))
+	}
+};
