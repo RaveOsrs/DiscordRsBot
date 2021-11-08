26400e279a0706195a139e97de0c8ead3e458cc0
Per Slegt <per.slegt@gmail.com>
2021-11-08 17:31:15 +0100

    embed

--- commands/profile.js
+++ commands/profile.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 const admin = require('firebase-admin');
 
 const DB = admin.database();
@@ -10,18 +11,30 @@ module.exports = {
         .addStringOption(option =>
             option.setName('rsn')
             .setDescription('User u want to check.')
-            .setRequired(false)),
+            .setRequired(true)),
 	async execute(interaction) {
-        let rsn;
-        if (!rsn) {
-            rsn = interaction.member.displayName.toLowerCase()
-        }
-        rsn = interaction.options.getString('rsn');
+        const rsn = interaction.options.getString('rsn');
 
         DB.ref('users/'+rsn).once('value').then(function(snapshot) {
+            const username = snapshot.val().userId.username;
+            const joined = new Date(snapshot.val().joined);
+            const referrals = snapshot.val().referrals.toString();
+            const rank = snapshot.val().progressionRank.toString();
             const wins = snapshot.val().compWins.toString();
             const id = snapshot.val().userId.id.toString();
-            interaction.reply(`Wins:${wins}, disc ID: ${id})`)
+            let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+            const profileEmbed = new MessageEmbed()
+                .setColor('#ffa500')
+                .setTitle(`${username}`)
+                .setDescription(`Joined: ${joined.toLocaleDateString('en-US', options)}\n
+                    Referrals: ${referrals}
+                    Rank: ${rank}
+                    Competition wins: ${wins}
+                    Discord ID: ${id}`)
+                .setTimestamp();
+
+            interaction.reply({ embeds: [profileEmbed] })
         })
 	}
 };
