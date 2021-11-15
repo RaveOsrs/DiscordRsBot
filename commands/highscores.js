fe038075541b4202bf7374c689f8545c0b92d891
Per Slegt <per.slegt@gmail.com>
2021-11-15 12:57:16 +0100

    more choices for highscores

--- commands/highscores.js
+++ commands/highscores.js
@@ -34,14 +34,71 @@ module.exports = {
             .addChoice('Farming', 'farming')
             .addChoice('Runecrafting', 'runecraft')
             .addChoice('Hunter', 'hunter')
-            .addChoice('Construction', 'construction')),
+            .addChoice('Construction', 'construction')
+            .addChoice('All Clues', 'clueall')
+            .addChoice('Beginner Clues', 'cluebeginner')
+            .addChoice('Easy Clues', 'clueeasy')
+            .addChoice('Medium Clues', 'cluemedium')
+            .addChoice('Hard Clues', 'cluehard')
+            .addChoice('Elite Clues', 'clueelite')
+            .addChoice('Master Clues', 'cluemaster')
+            .addChoice('Last man standing', 'lms')
+            .addChoice('Abyssal Sire', 'sire')
+            .addChoice('Alchemical Hydra', 'hydra')
+            .addChoice('Barrows', 'barrows')
+            .addChoice('Bryophyta', 'bryophyta')
+            .addChoice('Callisto', 'callisto')
+            .addChoice('Cerberus', 'cerberus')
+            .addChoice('Chambers of Xeric', 'cox')
+            .addChoice('Chambers of Xeric Challange mode', 'coxcm')
+            .addChoice('Chaos Elemental', 'chaoselemental')
+            .addChoice('Chaos Fanatic', 'chaosfanatic')
+            .addChoice('Commander Zilyana', 'commanderzilyana')
+            .addChoice('Corporeal Beast', 'corporealbeast')
+            .addChoice('Crazy Archaeologist', 'crazyarchaeologist')
+            .addChoice('Dagannoth Prime', 'prime')
+            .addChoice('Dagannoth Rex', 'rexbro')
+            .addChoice('Dagannoth Supreme', 'supreme')
+            .addChoice('Deranged Archaeologist', 'derangedarchaeologist')
+            .addChoice('General Graardor', 'generalgraardor')
+            .addChoice('Giant Mole', 'giantmole')
+            .addChoice('Grotesque Guardians', 'grotesqueguardians')
+            .addChoice('Hespori', 'hespori')
+            .addChoice('Kalphite Queen', 'kq')
+            .addChoice('King Black Dragon', 'kbd')
+            .addChoice('Kraken', 'kraken')
+            .addChoice('Kree Arra', 'kreearra')
+            .addChoice('Krill Tsutsaroth', 'kriltsutsaroth')
+            .addChoice('Mimic', 'mimic')
+            .addChoice('Obor', 'obor')
+            .addChoice('Sarachnis', 'sarachnis')
+            .addChoice('Scorpia', 'scorpia')
+            .addChoice('Skotizo', 'skotizo')
+            .addChoice('The Gauntlet', 'gauntlet')
+            .addChoice('Corrupted Gauntlet', 'gauntlethard')
+            .addChoice('Theatre of Blood', 'tob')
+            .addChoice('Thermonuclear Smoke Devil', 'smokedevil')
+            .addChoice('TzKal-Zuk', 'zuk')
+            .addChoice('TzTok-Jad', 'jad')
+            .addChoice('Venenatis', 'venenatis')
+            .addChoice('Vetion', 'vetion')
+            .addChoice('Vorkath', 'vorkath')
+            .addChoice('Wintertodt', 'wintertodt')
+            .addChoice('Zalcano', 'zalcano')
+            .addChoice('Zulrah', 'zulrah')
+            .addChoice('Nightmare', 'nightmare')
+            .addChoice('Soul Wars Zeal', 'zeal')
+            .addChoice('Tempoross', 'tempoross')
+            .addChoice('Theatre of Blood Hard Mode', 'tobcm')
+            .addChoice('Bounty Hunter - Hunter', 'bhh')
+            .addChoice('Bounty Hunter - Rogue', 'bhr')
+            .addChoice('Phosanis Nightmare', 'phosanis')),
 	async execute(interaction) {
         const category = interaction.options.getString('category');
         try {
             fetch(`https://templeosrs.com/api/skill_hiscores.php?group=${groupID}&skill=${category}&count=10`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 let string = "";
                 for (var key in data.data.players) {
                     if (data.data.players.hasOwnProperty(key)) {
@@ -49,12 +106,6 @@ module.exports = {
                         string += numberWithCommas(data.data.players[key].xp) + "\n";
                     }
                 }
-                /*for (let i = 0; i < 10; i++) {
-                    objString = ""+ i + 1;
-                    console.log(objString);
-                    string += `**${data.data.players[].username}**: `;
-                    string += numberWithCommas(data.data.players[i.toString()].xp) + "\n";
-                }*/
                 const resultEmbed = new MessageEmbed()
                     .setColor('#ffa500')
                     .setTitle(`${data.data.skill} highscores`)
