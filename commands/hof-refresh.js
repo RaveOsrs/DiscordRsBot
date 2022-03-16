77da7cfed1e7f8be42aa8d9f79cd53b7c209b05b
Per Slegt <per.slegt@gmail.com>
2022-03-16 18:59:52 +0100

    fix

--- commands/hof-refresh.js
+++ commands/hof-refresh.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const {Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
 const admin = require('firebase-admin');
 
