09f9cb550cc82c093b89cf3cddefea243b254d91
Per Slegt <per.slegt@gmail.com>
2022-04-24 13:56:58 +0200

    heroku

--- commands/hof-refresh.js
+++ commands/hof-refresh.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+/*const { SlashCommandBuilder } = require('@discordjs/builders');
 const {Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],partials: ["CHANNEL"] });
 const admin = require('firebase-admin');
@@ -27,4 +27,4 @@ module.exports = {
             })
         })
 	},
-};
+};*/
